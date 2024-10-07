bca313b41928012fa93f8764c04c4da07041349b
Vishnu Babu Guturu <vguturu@ucsd.edu>
2024-10-07 14:20:25 -0700

    Exercise 4.1.1 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   <div className='app-container'>
+     <div className="note-individual">
+       <button>
+         x
+       </button>
+       <h2> 1st Note Title </h2>
+       <p> 1st Note Content </p>
+       <p> 1st Note Label </p>
+     </div>
+   </div>
+
+ );
 }
 
 export default App;
